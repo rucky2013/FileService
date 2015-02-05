2fb438f3bc218a09b22ac23fc92f963d1b9f7516
jiaoyiping <jiaoyiping@gmail.com>
2015-02-06 00:43:02 +0800

    大体列出了REST接口的定义(没有具体实现)

--- src/main/java/com/jiaoyiping/App.java
+++ src/main/java/com/jiaoyiping/App.java
@@ -1,11 +1,9 @@
 package com.jiaoyiping;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,11 +13,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
- * Hello world!
+ * 设计说明：1.小文件存储在MongoDB里,生成一个全局唯一的串码来表示
+ *          2.外系统使用本服务做文件存储引擎(上传之后保存文件的串码)
+ *          3.外系统使用REST接口对文件进行上传、下载、查询、删除
  */
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 @RequestMapping("/file")
 public class App extends SpringBootServletInitializer {
 
@@ -40,4 +38,22 @@ public class App extends SpringBootServletInitializer {
         out.flush();
         out.close();
     }
+    //根据文件标识符来查询文件()
+    @RequestMapping(value = "/get",method = RequestMethod.GET)
+    public void getFile(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    //下载文件
+    @RequestMapping(value = "/download",method = RequestMethod.GET)
+    public String downloadFile(){
+        return null;
+    }
+
+    //根据文件标识符来删除文件
+    @RequestMapping(value = "/delete",method = RequestMethod.GET)
+    public void deleteFile(){
+
+    }
+
 }
